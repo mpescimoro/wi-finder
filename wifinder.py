d7b02231879c33377c5b99944df06997a9eeb29b
Christian Brosche <c.brosche@gmail.com>
2014-07-05 11:00:11 +0200

    Added a kind of buffer to account for inaccurate pings

--- wifinder.py
+++ wifinder.py
@@ -22,6 +22,7 @@ import os
 import sys
 import nmap                         # import nmap.py
 import time
+import re
 
 try:
     nm = nmap.PortScanner()         # instance of nmap.PortScanner
@@ -32,8 +33,11 @@ except:
     print("Unexpected error:", sys.exc_info()[0])
     sys.exit(0)
 
+hostList = []
+gracePeriod = 7
+
 def seek():                         # function to scan the network
-    count = 0
+    curHosts = []
     nm.scan(hosts = '192.168.1.0/24', arguments = '-n -sP -PE -T5')
     # executes a ping scan
 
@@ -42,25 +46,55 @@ def seek():                         # function to scan the network
     # system time
     
     for host in nm.all_hosts():
-        count += 1
         try:
             mac = nm[host]['addresses']['mac']
         except:
             mac = 'no data'
-        host_list = (host, mac)
-        print('Host: %s [%s]' % (host, mac))
-        
-    print('Number of hosts: ' + str(count))
-    return count                # returns count
+
+        curHosts.append((host,mac,gracePeriod))
+    
+    updateHostList(curHosts)
+
+    for host in hostList:
+        print('Host: %s\t[%s]\t%s' % (host[0], host[1], host[2]))
+
+    print('Number of hosts: ' + str(len(hostList)))
+    return len(hostList)                # returns count
+
+def updateHostList(curHosts):
+    global hostList
+    if hostList == []:
+        hostList = curHosts
+    else:
+        hostList = [(x[0],x[1],x[2]-1) for x in hostList]
+
+        # only the hosts that were new in this iteration
+        newList = [(x[0],x[1],x[2]) for x in curHosts if not (any(x[0]==y[0] for y in hostList))]
+
+        for host in newList:
+            hostList.append(host)
+
+        for host in hostList:
+            if any(host[0] == y[0] for y in curHosts):
+                hostList[hostList.index(host)] = (host[0],host[1],gracePeriod)
+
+        for host in hostList:
+            if host[2] <= 0:
+                hostList.remove(host)
+
+
 
 def beep():                         # no sound dependency
     print('\a')            
     
 if __name__ == '__main__':
     old_count = new_count = seek()
+
+    startCounter = gracePeriod
     
     # are there any new hosts?
-    while (new_count <= old_count):
+    while (new_count <= old_count) or startCounter >= 0:
+        startCounter -= 1
         time.sleep(1)               # increase to slow down the speed
         old_count = new_count
         new_count = seek()
